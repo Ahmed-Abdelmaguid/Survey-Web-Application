cb934aba52c26a5296c613517e5070e2cc53b37f
Ahmed-Abdelmaguid <hitch96@my.yorku.ca>
2021-01-12 15:25:16 +0200

    Removed dev.js from git ignore

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '835602394132-adfkqe2od5obrq967ap85vp5klgph4bc.apps.googleusercontent.com',
+    googleClientSecret: '6uiL1NGWEe5pD7YdlEtIFBdR',
+    mongoURI: 'mongodb+srv://hitch:nhsaty123@emaily.ztvlt.mongodb.net/user1?retryWrites=true&w=majority',
+    cookieKey: 'wjihdfcnwhedfaqneqalnd'
+};
