27922e1faf19f3baf3bddd6948d3b05772eb76e9
Ahmed-Abdelmaguid <hitch96@my.yorku.ca>
2021-03-06 18:30:05 -0500

    Changed the functional App component to class component to give it the ability to call an action creator

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,10 @@
 // if a file is exporting a class or a react component it will start with a cappital letter
 
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'
+import { connect } from 'react-redux';
+// to give certian components the ablility to call action creators
+import * as actions from '../actions';
 //browersRouter is as the brains of how  react-router and tels it how to behave 'looks at the current url and then changes the set of components visible on the screen
 //route is the react component that is used to set up a rule betweena certain route url and a set of components that will be visible on the screen   
 
@@ -11,22 +14,28 @@ const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
 
-const App = () => {
-    // create a fuctional component that returns jxs
-    return (
-        <div>
-            <BrowserRouter>
-                <div className="container">
-                    <Header />
-                    <Route exact path="/" component={Landing} />
-                    {/* exact ensures that component only appears when the path is exaclty the same */}
-                    <Route exact path="/surveys" component={Dashboard} />
-                    <Route path="/surveys/new" component={SurveyNew} />
-                </div>
-            </BrowserRouter>
-            {/* browserRouter can only have one child */}
-        </div>
-    );
-};
+class App extends Component {
+    // refactored app component to class component so that we can easily call an action creator to fetch the current user the instace our app loads
+    componentDidMount() {
+        this.props.fetchUser();
+    }
 
-export default App;
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <div className="container">
+                        <Header />
+                        <Route exact path="/" component={Landing} />
+                        {/* exact ensures that component only appears when the path is exaclty the same */}
+                        <Route exact path="/surveys" component={Dashboard} />
+                        <Route path="/surveys/new" component={SurveyNew} />
+                    </div>
+                </BrowserRouter>
+                {/* browserRouter can only have one child */}
+            </div>
+        );
+    }
+}
+
+export default connect(null, actions)(App);
