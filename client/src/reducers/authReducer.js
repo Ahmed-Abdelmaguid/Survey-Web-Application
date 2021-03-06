f20eab663a77676f9f182e30069a80cc1c72c291
Ahmed-Abdelmaguid <hitch96@my.yorku.ca>
2021-03-06 18:38:04 -0500

    dependencies added and console.log added for debugging in authReducer file

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,6 @@
 export default function (state = {}, action){
     // state object is resposible for this fuction
+    console.log(action);
     switch (action.type) {
         default:
             return state;
