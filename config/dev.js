e2b04bce0432da2b9f972247851327f08bd6974b
Ahmed-Abdelmaguid <hitch96@my.yorku.ca>
2021-03-11 15:11:48 -0500

    fixed dev

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    googleClientID: '835602394132-adfkqe2od5obrq967ap85vp5klgph4bc.apps.googleusercontent.com',
-    googleClientSecret: '6uiL1NGWEe5pD7YdlEtIFBdR',
-    mongoURI: 'mongodb+srv://hitch:nhsaty123@emaily.ztvlt.mongodb.net/user1?retryWrites=true&w=majority',
-    cookieKey: 'wjihdfcnwhedfaqneqalnd',
-    stripePublishableKey: 'pk_test_51ITcmbJ6aicqQzRHVgX6Op74OCbXETAsln9sazO2Pwyw3Obp5StsjDs7CrQuVPj0gLZOec2CHV5zg4c1JbULoIeD00Qf74Z8rF',
-    stripeSecretKey: 'sk_test_51ITcmbJ6aicqQzRHEVVtgbOyvLdsRapowQlvN8q0Y0rekb61ejfwfMPDvFr6LQN26FbwFWUARNU2kYCaC1ilKy3h00Er146gHc'
-};
