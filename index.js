d69bd716dd5b7d9e6c8909fb2c236b203045ebc6
Ahmed-Abdelmaguid <hitch96@my.yorku.ca>
2021-06-09 05:48:09 -0400

    Back end to front end routing in production

--- index.js
+++ index.js
@@ -24,5 +24,19 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    //Express will serve up production assets
+    //Like our main.js file, or main.css file
+    app.use(express.static('client/build')); //if any route request comes up that does not match any of the routes above then look for that route inside the build folder
+
+
+    //Express will serve up the index.html file
+    //if it doesnt recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
