5354ed3305ffebe82bf6327732dbb5f11abac680
Ahmed-Abdelmaguid <hitch96@my.yorku.ca>
2021-06-15 20:07:12 -0400

    Testing Survey validation

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,14 +2,16 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 
 const FIELDS = [
-    { label: 'Survey Title', name: 'title' },
-    { label: 'Subject Line', name: 'subject' },
-    { label: 'Email Body', name: 'body' },
-    { label: 'Recipient List', name: 'email' }
+    { label: 'Survey Title', name: 'title', noValueError: 'You must provide a title' },
+    { label: 'Subject Line', name: 'subject', noValueError: 'You must provide a subject' },
+    { label: 'Email Body', name: 'body', noValueError: 'You must provide a body' },
+    { label: 'Recipient List', name: 'email', noValueError: 'You must provide a email' }
 ];
 
 class SurveyForm extends Component {
@@ -24,13 +26,34 @@ class SurveyForm extends Component {
             <div>
                 <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
                     {this.renderFields()}
-                    <button type="submit">Submit</button>
+                    <Link to="/surveys" className="red btn-flat white-text">
+                        Cancel
+                    </Link>
+                    <button type="submit" className="teal btn-flat right white-text">
+                        Next
+                        <i className="material-icons right">done</i>
+                    </button>
                 </form>
             </div>
         );
     }
 }
 
+function validate(values) {
+    const errors = {};
+
+    FIELDS.forEach( ({ name, noValueError }) => {
+        if (!values[name]) {
+            errors[name] = noValueError;
+        }
+    });
+
+    errors.emails = validateEmails(values.emails || '');
+
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'surveyForm'
 })(SurveyForm);
---
+++ client/src/utils/validateEmails.js
@@ -0,0 +1,15 @@
+const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export default emails => {
+    const invalidEmails = emails
+    .split(',')
+    .map(email => email.trim())
+    .filter(email => re.test(email) === false);
+    //map works by taking the value out of the array then whatever we return will be added a new array then returns the new array   
+
+    if (invalidEmails.length) {
+        return `These emails are invalid: ${invalidEmails}`;
+    }
+
+    return;
+};
